434b09e178a1a9170a2c83b828b90bc3c5fe54ca
satyen mori <satyenmori@gmail.com>
2024-03-08 17:02:34 +0530

    deleteuser

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,10 +4,28 @@ const Home = () => {
   const [userData, setUserData] = useState([]);
 
   useEffect(() => {
-    const loggedinUser = JSON.parse(localStorage.getItem("loggedinUsers")) || [];
+    const loggedinUser =
+      JSON.parse(localStorage.getItem("loggedinUsers")) || [];
 
     setUserData(loggedinUser);
   }, []);
+
+  // Delete User
+
+  const deleteuser = (id) => {
+    let loggedInUsers = JSON.parse(localStorage.getItem("loggedinUsers")) || [];
+
+    const deleteUserid = loggedInUsers.findIndex((user) => user.id === id);
+    if (deleteUserid !== -1) {
+      loggedInUsers.splice(deleteUserid, 1);
+      localStorage.setItem("loggedinUsers", JSON.stringify(loggedInUsers));
+      setUserData(loggedInUsers);
+      alert("User Delete Successfully");
+    } else {
+      alert("User Not Found");
+    }
+  };
+
   return (
     <>
       <table class="table container w-50  mt-5 ">
@@ -18,6 +36,7 @@ const Home = () => {
             <th scope="col">Email</th>
             <th scope="col">Password</th>
             <th scope="col">Date & Time</th>
+            <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
@@ -28,6 +47,14 @@ const Home = () => {
               <td>{user.email}</td>
               <td>{user.password}</td>
               <td>{user.time}</td>
+              <td>
+                <button className="edit">
+                  <i className="fa-regular fa-pen-to-square"></i>
+                </button>
+                <button className="delete" onClick={() => deleteuser(user.id)}>
+                  <i className="fa-solid fa-trash fa"></i>
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
