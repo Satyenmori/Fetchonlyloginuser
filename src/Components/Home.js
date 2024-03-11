526827f34e520b2a180189a4e329dd8b932b9f9c
satyen mori <satyenmori@gmail.com>
2024-03-11 13:31:21 +0530

    loggin data get in store

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,12 @@
+import { useAuth } from "../store/store";
+
 const Home = () => {
+  const{user}=useAuth();
   return (
     <>
-      <div className="container card h-50 w-25 mt-5 ">
-        <h2>This Is Home Page</h2>
+      <div className="container card h-50 w-50 mt-5 ">
+        <h2>Hello, <span className="username">{user.username}</span> </h2>
+        <h4>Welcome Our WebSite</h4>
       </div>
     </>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,46 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState("");
+  const AuthorizationToken = `Bearer ${token}`;
+
+  // Jwt token StoreIn LocalStore
   const storeTokenInLS = (serverToken) => {
     return localStorage.setItem("token", serverToken);
   };
 
-  return <AuthContext.Provider value={{storeTokenInLS}}>{children}</AuthContext.Provider>;
+  // Get Loggin User Data
+
+  const userAuthentication = async () => {
+    try {
+      const response = await fetch("http://localhost:5151/user/", {
+        method: "GET",
+        headers: { Authorization: AuthorizationToken },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setUser(data.userData);
+      } else {
+        console.log("Error fetching user data");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    userAuthentication();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ storeTokenInLS, user, AuthorizationToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => {
