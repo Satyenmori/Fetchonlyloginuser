6b6049fcb01700a5de9c834c98b09b609e231feb
satyen mori <satyenmori@gmail.com>
2024-03-26 13:25:30 +0530

    Food and category Fetch

---
+++ src/Components/Home.jsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [foods, setFood] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const fetchFood = async () => {
+    try {
+      const response = await fetch("http://localhost:5151/food/getfood");
+      const data = await response.json();
+      setFood(data);
+      console.log(foods);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchCategory = async () => {
+    try {
+      const response = await fetch("http://localhost:5151/category/");
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchFood();
+    fetchCategory();
+  }, []);
+  return (
+    <>
+      <div className="menu-box">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="heading-title text-center mb-3 mt-2">
+                <h2 >Special Menu</h2>
+              </div>
+            </div>
+          </div>
+          <div className="row inner-menu-box">
+            <div className="col-3">
+              <div
+                className="nav flex-column nav-pills"
+                id="v-pills-tab"
+                role="tablist"
+                aria-orientation="vertical"
+              >
+                <a
+                  className="nav-link active"
+                  id="v-pills-home-tab"
+                  data-toggle="pill"
+                  href="#v-pills-home"
+                  role="tab"
+                  aria-controls="v-pills-home"
+                  aria-selected="true"
+                >
+                  All
+                </a>
+                {categories.map((category) => (
+                  <a key={category.id} className="nav-link">
+                    {category.label}
+                  </a>
+                ))}
+              </div>
+            </div>
+            <div className="col-9">
+              <div className="tab-content" id="v-pills-tabContent">
+                <div
+                  className="tab-pane fade show active"
+                  id="v-pills-home"
+                  role="tabpanel"
+                  aria-labelledby="v-pills-home-tab"
+                >
+                  <div className="row">
+                    {foods &&
+                      foods.map((food) => (
+                        <div
+                          key={food.id}
+                          className="col-lg-4 col-md-6 wow fadeInUp"
+                          data-wow-delay="0.1s"
+                        >
+                          <div className="room-item shadow rounded overflow-hidden">
+                            <div className="position-relative">
+                              <img
+                                className="img-fluid room-image"
+                                src={food.img}
+                                alt="thumbnail"
+                              />
+                              <small className="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">
+                                $ {food.price}
+                              </small>
+                            </div>
+                            <div className="p-4 mt-2">
+                              <div className="d-flex justify-content-between mb-3">
+                                <h5 className="mb-0">{food.name}</h5>
+                                <div className="ps-2">
+                                  <small className="fa fa-star text-primary">
+                                    {food.rating}
+                                  </small>
+                                </div>
+                              </div>
+                              <p className="text-body mb-3 scroll">
+                                {food.desc}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
